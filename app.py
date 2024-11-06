9a16b6e359911f0ebb20b6f45c03b06deee4022c
Ajay Jayanth <kunhu2004@gmail.com>
2024-11-05 18:24:54 -0600

    flask init

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from flask_cors import CORS
+
+name = 'Budget Buddy'
+app = Flask(name) 
+CORS(app)
+
+app.config.update( 
+    SECRET_KEY='dev'
+)
+
+@app.route('/')
+def index_function(): 
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='localhost', port=3000, threaded=True)
